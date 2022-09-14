db4c3c0fb63f66be74bc25ff5cd3dfd30f3af1d2
Daniel Neri <danielnerijr@gmail.com>
2022-09-14 08:43:55 -0300

    Exercicio 2

---
+++ Exercicios/Exercicios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor raio do circulo:");
+            double raio = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de Pi");
+            double valor = double.Parse(Console.ReadLine());
+            
+            double total;
+
+            total = valor * (raio * raio);
+
+            Console.WriteLine("O valor da área é de " + total.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
